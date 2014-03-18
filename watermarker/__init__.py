77e81def12b210869c70ef9e6363688f8a180317
Olof Sjöbergh <olofsj@gmail.com>
2014-03-18 11:46:05 +0100

    Add options for text position, relative margins, outline and antialiasing.

--- watermarker/__init__.py
+++ watermarker/__init__.py
@@ -28,22 +28,64 @@ def ReduceOpacity(im, opacity):
     im.putalpha(alpha)
     return im
 
-def Imprint(im, inputtext, font=None, color=None, opacity=0.6, margin=(30,30)):
+
+def Imprint(im, inputtext, font=None, color=None, outline=None, opacity=0.6, margin=(30,30),
+            position='bottom-left', antialias=True):
     """
     imprints a PIL image with the indicated text in lower-right corner
     """
     if im.mode != "RGBA":
         im = im.convert("RGBA")
-    textlayer = Image.new("RGBA", im.size, (0,0,0,0))
+
+    # For anti-aliasing use twice the size and scale down
+    size = im.size
+    if antialias:
+        size = [2*s for s in size]
+
+    textlayer = Image.new("RGBA", size, (0,0,0,0))
     textdraw = ImageDraw.Draw(textlayer)
     textsize = textdraw.textsize(inputtext, font=font)
-    textpos = [im.size[i]-textsize[i]-margin[i] for i in [0,1]]
+
+    # Use relative margins for values < 1
+    margin = [round(margin[i]*size[i]) if margin[i] < 1 else margin[i] for i in [0,1]]
+
+    # Calculate positions for text from position and margins
+    textpos = [0, 0]
+    if 'left' in position:
+        textpos[0] = margin[0]
+    else:
+        textpos[0] = size[0]-textsize[0]-margin[0]
+    if 'top' in position:
+        textpos[1] = margin[1]
+    else:
+        textpos[1] = size[1]-textsize[1]-margin[1]
+
+    # Draw an outline of the text if specified
+    if outline:
+        s = 2 if antialias else 1
+        textdraw.text((textpos[0], textpos[1]-s), inputtext, font=font, fill=outline)
+        textdraw.text((textpos[0], textpos[1]+s), inputtext, font=font, fill=outline)
+        textdraw.text((textpos[0]-s, textpos[1]), inputtext, font=font, fill=outline)
+        textdraw.text((textpos[0]+s, textpos[1]), inputtext, font=font, fill=outline)
+        textdraw.text((textpos[0]-s, textpos[1]-s), inputtext, font=font, fill=outline)
+        textdraw.text((textpos[0]+s, textpos[1]-s), inputtext, font=font, fill=outline)
+        textdraw.text((textpos[0]-s, textpos[1]+s), inputtext, font=font, fill=outline)
+        textdraw.text((textpos[0]+s, textpos[1]+s), inputtext, font=font, fill=outline)
+
+    # Draw the text
     textdraw.text(textpos, inputtext, font=font, fill=color)
+
+    if antialias:
+        textlayer = textlayer.resize(im.size, resample=Image.ANTIALIAS)
+
     if opacity != 1:
-        textlayer = ReduceOpacity(textlayer,opacity)
+        textlayer = ReduceOpacity(textlayer, opacity)
+
     return Image.composite(textlayer, im, textlayer)
 
-def watermark(image, text, font_path, font_scale=None, font_size=None, color=(0,0,0), opacity=0.6, margin=(30, 30)):
+
+def watermark(image, text, font_path, font_scale=None, font_size=None, color=(0,0,0), outline=(0,0,0),
+              opacity=0.6, margin=(30, 30), position='bottom-left', antialias=True):
     """
     image - PIL Image instance
     text - text to add over image
@@ -57,6 +99,10 @@ def watermark(image, text, font_path, font_scale=None, font_size=None, color=(0,
         font_size = int(font_scale*height)
     elif not (font_size or font_scale):
         raise ImpropertlyConfigured("You should provide font_scale or font_size option")
-    font=ImageFont.truetype(font_path, font_size)
-    im0 = Imprint(image, text, font=font, opacity=opacity, color=color, margin=margin)
+
+    if antialias:
+        font_size = 2 * font_size
+
+    font = ImageFont.truetype(font_path, font_size)
+    im0 = Imprint(image, text, font=font, opacity=opacity, color=color, outline=outline, margin=margin, position=position, antialias=antialias)
     return im0
